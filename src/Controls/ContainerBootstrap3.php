230bdb151db23c67fa72bf42bf7006a2b3ca7239
George Benjamin-Schonberger <george.benjamin@gmail.com>
2018-09-12 13:36:02 +0300

    VERSION-2_2_6_3 - Controls hint support added

--- src/Controls/ContainerBootstrap3.php
+++ src/Controls/ContainerBootstrap3.php
@@ -11,70 +11,73 @@
  * @version    2.2.0.0
  * @filesource
  */
-    namespace NETopes\Core\Controls;
-	/**
-	 * Control container class file
-	 *
-	 * Control container implementation for Bootstrap3
-	 *
-	 * @package  NETopes\Core\Controls
-	 * @access   public
-	 */
-	class ContainerBootstrap3 implements IControlContainer {
-		/**
-		 * @var object Control instance
-		 */
-		protected $control;
-		/**
-		 * Control container class constructor
-		 *
-		 * @param $control
-		 * @access public
-		 */
-		public function __construct($control) {
-			$this->control = $control;
-		}//END public function __construct
-		/**
-		 * description
-		 *
-		 * @param string $content
-		 * @return string
-		 * @access public
-		 */
-		public function GetHtml($content) {
-			if(!$this->control->container && $this->control->no_label) {
-				$result = $content;
-			} else {
-				$result = '';
-				$c_label_cols = 0;
-				$c_label = '';
-				$c_class = is_string($this->control->container_class) && strlen(trim($this->control->container_class)) ? $this->control->container_class : '';
-				if(!$this->control->no_label) {
-					if($this->control->labelposition=='top') {
-						$c_label_cols = 12;
-					} else {
-						$c_label_cols = is_numeric($this->control->label_cols) && $this->control->label_cols>0 && $this->control->label_cols<12 ? $this->control->label_cols : 2;
-					}//if($this->control->labelposition=='top')
-					$llabelclass = 'control-label col-md-'.$c_label_cols.(strlen($this->control->labelclass) ? ' '.$this->control->labelclass : '');
-					$lrequired = $this->control->required ? '<span class="clsMarkerRequired"></span>' : '';
-					if(strlen($this->control->size)) { $llabelclass .= ' label-'.$this->control->size; }
-					$c_label = "\t\t".'<label class="'.$llabelclass.'" for="'.$this->control->tagid.'">'.$this->control->label.$lrequired.'</label>'."\n";
-				}//if(!$this->control->no_label)
-				if(!$this->control->no_label && $this->control->labelposition=='top') {
-					$c_cols = is_numeric($this->control->cols) && $this->control->cols>0 && $this->control->cols<=12 ? $this->control->cols : 12;
-				} else {
-					$c_cols = is_numeric($this->control->cols) && $this->control->cols>0 && $this->control->cols<=(12-$c_label_cols) ? $this->control->cols : (12-$c_label_cols);
-				}//if(!$this->control->no_label && $this->control->labelposition=='top')
-				if($this->control->container) { $result .= "\t".'<div class="form-group'.(strlen($c_class) ? ' '.$c_class : '').'">'."\n"; }
-				$result .= $c_label;
-				// if($this->control->labelposition!='top') { }
-				$result .= "\t\t".'<div class="col-md-'.$c_cols.'">'."\n";
-				$result .= "\t\t\t".$content."\n";
-				$result .= "\t\t".'</div>'."\n";
-				// if($this->control->labelposition!='top') { }
-				if($this->control->container) { $result .= "\t".'</div>'."\n"; }
-			}//if(!$this->control->container && $this->control->no_label)
-			return $result;
-		}//END public function GetHtml
-	}//END class ContainerBootstrap3 implements IControlContainer
+namespace NETopes\Core\Controls;
+/**
+ * Control container class file
+ *
+ * Control container implementation for Bootstrap3
+ *
+ * @package  NETopes\Core\Controls
+ * @access   public
+ */
+class ContainerBootstrap3 implements IControlContainer {
+    /**
+     * @var object Control instance
+     */
+    protected $control;
+    /**
+     * Control container class constructor
+     *
+     * @param $control
+     * @access public
+     */
+    public function __construct($control) {
+        $this->control = $control;
+    }//END public function __construct
+    /**
+     * description
+     *
+     * @param string $content
+     * @return string
+     * @access public
+     */
+    public function GetHtml($content) {
+        if(!$this->control->container && $this->control->no_label) {
+            $result = $content;
+        } else {
+            $result = '';
+            $c_label_cols = 0;
+            $c_label = '';
+            $c_class = is_string($this->control->container_class) && strlen(trim($this->control->container_class)) ? $this->control->container_class : '';
+            if(!$this->control->no_label) {
+                if($this->control->labelposition=='top') {
+                    $c_label_cols = 12;
+                } else {
+                    $c_label_cols = is_numeric($this->control->label_cols) && $this->control->label_cols>0 && $this->control->label_cols<12 ? $this->control->label_cols : 2;
+                }//if($this->control->labelposition=='top')
+                $llabelclass = 'control-label col-md-'.$c_label_cols.(strlen($this->control->labelclass) ? ' '.$this->control->labelclass : '');
+                $lrequired = $this->control->required ? '<span class="clsMarkerRequired"></span>' : '';
+                if(strlen($this->control->size)) { $llabelclass .= ' label-'.$this->control->size; }
+                $c_label = "\t\t".'<label class="'.$llabelclass.'" for="'.$this->control->tagid.'">'.$this->control->label.$lrequired.'</label>'."\n";
+            }//if(!$this->control->no_label)
+            if(!$this->control->no_label && $this->control->labelposition=='top') {
+                $c_cols = is_numeric($this->control->cols) && $this->control->cols>0 && $this->control->cols<=12 ? $this->control->cols : 12;
+            } else {
+                $c_cols = is_numeric($this->control->cols) && $this->control->cols>0 && $this->control->cols<=(12-$c_label_cols) ? $this->control->cols : (12-$c_label_cols);
+            }//if(!$this->control->no_label && $this->control->labelposition=='top')
+            if($this->control->container) { $result .= "\t".'<div class="form-group'.(strlen($c_class) ? ' '.$c_class : '').'">'."\n"; }
+            $result .= $c_label;
+            // if($this->control->labelposition!='top') { }
+            $result .= "\t\t".'<div class="col-md-'.$c_cols.'">'."\n";
+            $result .= "\t\t\t".$content."\n";
+            if(is_string($this->control->field_hint) && strlen($this->control->field_hint)) {
+                $result .= "\t\t\t".'<p'.(strlen($this->control->tagid) ? ' id="'.$this->control->tagid.'_hint"' : '').' class="help-block">'.$this->control->field_hint.'</p>'."\n";
+            }//if(is_string($this->control->field_hint) && strlen($this->control->field_hint))
+            $result .= "\t\t".'</div>'."\n";
+            // if($this->control->labelposition!='top') { }
+            if($this->control->container) { $result .= "\t".'</div>'."\n"; }
+        }//if(!$this->control->container && $this->control->no_label)
+        return $result;
+    }//END public function GetHtml
+}//END class ContainerBootstrap3 implements IControlContainer
 ?>
