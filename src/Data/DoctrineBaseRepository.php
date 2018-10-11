21e17641042b34aea288bd5a26194481551c0d58
George Benjamin-Schonberger <george.benjamin@gmail.com>
2018-10-11 13:15:15 +0300

    VERSION-2_2_8_9 - Doctrine base repository fix

--- src/Data/DoctrineBaseRepository.php
+++ src/Data/DoctrineBaseRepository.php
@@ -11,9 +11,20 @@ class DoctrineBaseRepository extends EntityRepository {
 	public function getSearchResults(string $term,array $targets = [],array $params = [],int $rowNum = 10) {
         $qb = $this->createQueryBuilder('e');
 		$words = str_word_count($term, 1, '1234567890');
+
 		foreach($params as $pn=>$pv) {
-			$qb->andWhere($qb->expr()->eq('e.'.$pn,$pv));
-		}//END foreach
+            if(is_array($pv)) {
+               foreach($pv as $pvc=>$pvv) {
+                   $operators = $this->getOperators();
+                   $operator = get_array_value($operators,strtolower($pvc),'','is_string');
+                   if(!strlen($operator)) { continue; }
+                   $qb->andWhere($qb->expr()->$operator('e.'.$pn,$pvv));
+               }//END foreach
+           } else {
+               $qb->andWhere($qb->expr()->eq('e.'.$pn,$pv));
+           }//if(is_array($pv))
+        }//END foreach
+
         foreach($words as $k=>$word) {
             $xor = $qb->expr()->orX();
             foreach($targets as $target) {
