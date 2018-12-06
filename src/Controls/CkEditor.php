88c0aed81da350ae7d71b43628a636d191833ef9
George Benjamin-Schonberger <george.benjamin@gmail.com>
2018-12-06 11:42:42 +0200

    VERSION-2_2_10_10 - CkEditor [extraconfig] option can be passed as array or string

--- src/Controls/CkEditor.php
+++ src/Controls/CkEditor.php
@@ -11,52 +11,61 @@
  * @version    2.1.0.0
  * @filesource
  */
-    namespace NETopes\Core\Controls;
-    use NApp;
-    use PAF\AppException;
-	/**
-	 * ClassName description
-	 *
-	 * long_description
-	 *
-	 * @package  NETopes\Controls
-	 * @access   public
-	 */
-	class CkEditor extends Control {
-		public $extraconfig = NULL;
-		/**
-		 * description
-		 *
-		 * extraconfig:
-		 * - toolbarStartupExpanded: false (hide toolbars on initialization)
-		 *
-		 * @return void
-		 * @access public
-		 * @throws \PAF\AppException
-		 */
-		protected function SetControl() {
-			if(!strlen($this->tagid)) { throw new AppException('Invalid tag ID!',E_ERROR,1); }
-			$lextratagparam = strlen($this->extratagparam)>0 ? ' '.$this->extratagparam : '';
-			$result = "\t\t".'<textarea'.$this->GetTagId().$this->GetTagClass('textarea').$lextratagparam.'>'.$this->value.'</textarea>'."\n";
-			$lwidth = $this->width ? (is_numeric($this->width) ? ','.$this->width : ',\''.$this->width.'\'') : '';
-			$lheight = $this->height ? (is_numeric($this->height) ? ','.$this->height : ',\''.$this->height.'\'') : '';
-			$lextraconfig = (is_string($this->extraconfig) && strlen($this->extraconfig)) ? '{'.$this->extraconfig.'}' : 'undefined';
-			if(NApp::ajax() && is_object(NApp::arequest())) {
-				NApp::arequest()->ExecuteJs("CreateCkEditor('{$this->phash}','{$this->tagid}',false,".$lextraconfig.$lwidth.$lheight.");");
-			} else {
-				$result .= "\t\t"."<script type=\"text/javascript\">CreateCkEditor('{$this->phash}','{$this->tagid}',false,".$lextraconfig.$lwidth.$lheight.");</script>\n";
-			}//if(NApp::ajax() && is_object(NApp::arequest()))
-			return $result;
-		}//END protected function SetControl
-		/**
-		 * description
-		 *
-		 * @param bool $all
-		 * @return void
-		 * @access public
-		 */
-		public function GetDestroyJsCommand($all = FALSE) {
-			return $all ? "DestroyCkEditor('{$this->phash}');" : "DestroyCkEditor('{$this->phash}','{$this->tagid}',false);";
-		}//END public function GetDestroyJsCommand
-	}//END class CkEditor extends Control
-?>
+namespace NETopes\Core\Controls;
+use NApp;
+use PAF\AppException;
+
+/**
+ * ClassName description
+ *
+ * long_description
+ *
+ * @package  NETopes\Controls
+ * @access   public
+ */
+class CkEditor extends Control {
+    /**
+     * @var array|string|null
+     */
+    public $extraconfig = NULL;
+    /**
+     * description
+     *
+     * extraconfig:
+     * - toolbarStartupExpanded: false (hide toolbars on initialization)
+     *
+     * @return string|null
+     * @access public
+     * @throws \PAF\AppException
+     */
+    protected function SetControl() {
+        if(!strlen($this->tagid)) { throw new AppException('Invalid tag ID!',E_ERROR,1); }
+        $lextratagparam = strlen($this->extratagparam)>0 ? ' '.$this->extratagparam : '';
+        $result = "\t\t".'<textarea'.$this->GetTagId().$this->GetTagClass('textarea').$lextratagparam.'>'.$this->value.'</textarea>'."\n";
+        $lwidth = $this->width ? (is_numeric($this->width) ? ','.$this->width : ',\''.$this->width.'\'') : '';
+        $lheight = $this->height ? (is_numeric($this->height) ? ','.$this->height : ',\''.$this->height.'\'') : '';
+        $lextraconfig = 'undefined';
+        if(is_array($this->extraconfig)) {
+            try {
+                $lextraconfig = json_encode($this->extraconfig);
+            } catch(\Exception $je) {
+                NApp::_Elog($je->getMessage());
+                $lextraconfig = 'undefined';
+            }//END try
+        } elseif(is_string($this->extraconfig) && strlen($this->extraconfig)) {
+            $lextraconfig = '{'.trim($this->extraconfig,'}{').'}';
+        }//if(is_array($this->extraconfig))
+        NApp::_ExecJs("CreateCkEditor('{$this->phash}','{$this->tagid}',false,".$lextraconfig.$lwidth.$lheight.");");
+        return $result;
+    }//END protected function SetControl
+    /**
+     * description
+     *
+     * @param bool $all
+     * @return string
+     * @access public
+     */
+    public function GetDestroyJsCommand($all = FALSE) {
+        return $all ? "DestroyCkEditor('{$this->phash}');" : "DestroyCkEditor('{$this->phash}','{$this->tagid}',false);";
+    }//END public function GetDestroyJsCommand
+}//END class CkEditor extends Control
