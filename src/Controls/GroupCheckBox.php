afc01be7f9a2780e4b3e691969e4e6c611708994
George Benjamin-Schonberger <george.benjamin@gmail.com>
2019-06-10 18:39:10 +0300

    FEATURE - GroupCheckBox multi-select support added (thanks to Andrei Ionescu for contribution)

--- src/Controls/GroupCheckBox.php
+++ src/Controls/GroupCheckBox.php
@@ -11,23 +11,28 @@
  * @filesource
  */
 namespace NETopes\Core\Controls;
+use NApp;
 use NETopes\Core\AppConfig;
 use NETopes\Core\AppSession;
-use NETopes\Core\Data\DataProvider;
 use NETopes\Core\Data\DataSet;
 use NETopes\Core\Data\DataSourceHelpers;
 use NETopes\Core\Data\VirtualEntity;
-use NApp;
 use Translate;
 
 /**
  * GroupCheckBox class
  * Control class for group checkbox (radio button alternative)
  *
+ * @property string|null active_state
+ * @property string|null state_field
+ * @property string|null relation
+ * @property string|null label_field
+ * @property string|null id_field
  * @package  Hinter\NETopes\Controls
  */
 class GroupCheckBox extends Control {
     use TControlDataSource;
+    const MULTI_VALUE_SEPARATOR='|';
     /**
      * @var    array Elements data source
      */
@@ -48,6 +53,14 @@ class GroupCheckBox extends Control {
      * @var    mixed Initial value
      */
     public $value=NULL;
+    /**
+     * @var    bool Initial value
+     */
+    public $multiple=FALSE;
+    /**
+     * @var    bool Initial value
+     */
+    public $multiResult=TRUE;
 
     /**
      * Control class constructor
@@ -74,43 +87,59 @@ class GroupCheckBox extends Control {
         if(is_null($this->items)) {
             $this->items=$this->LoadData($this->data_source);
         }
-        $idfield=is_string($this->id_field) && strlen($this->id_field) ? $this->id_field : 'id';
-        $labelfield=is_string($this->label_field) && strlen($this->label_field) ? $this->label_field : 'name';
+        $idField=is_string($this->id_field) && strlen($this->id_field) ? $this->id_field : 'id';
+        $labelField=is_string($this->label_field) && strlen($this->label_field) ? $this->label_field : 'name';
         $relation=is_string($this->relation) && strlen($this->relation) ? $this->relation : '';
-        $statefield=is_string($this->state_field) && strlen($this->state_field) ? $this->state_field : NULL;
-        $activestate=is_string($this->active_state) && strlen($this->active_state) ? $this->active_state : '1';
+        $stateField=is_string($this->state_field) && strlen($this->state_field) ? $this->state_field : NULL;
+        $activeState=is_string($this->active_state) && strlen($this->active_state) ? $this->active_state : '1';
         $this->ProcessActions();
         $result='<div id="'.$this->tag_id.'-container" class='.$this->GetTagClass('clsGCKBContainer',TRUE).'">'."\n";
-        $result.="\t".'<input type="hidden" '.$this->GetTagId(TRUE).$this->GetTagClass().$this->GetTagActions().' value="'.$this->value.'">'."\n";
+        if($this->multiple && $this->multiResult) {
+            $hiddenTagSufix='_value';
+            $result.="\t".'<input type="hidden" '.$this->GetTagId(FALSE,$hiddenTagSufix).$this->GetTagClass(NULL,TRUE).$this->GetTagActions().' value="'.$this->GetValue().'" />'."\n";
+        } else {
+            $hiddenTagSufix='';
+            $result.="\t".'<input type="hidden" '.$this->GetTagId(TRUE).$this->GetTagClass().$this->GetTagActions().' value="'.$this->GetValue().'" />'."\n";
+        }//if($this->multiple && $this->multiResult)
         $ul_class='clsGCKBList '.(strtolower($this->orientation)==='vertical' ? 'oVertical' : 'oHorizontal');
         $result.="\t".'<ul class="'.$ul_class.'">'."\n";
-        // $items = $this->items['data'] ?? $this->items;
         if(is_object($this->items) && $this->items->count()) {
             foreach($this->items as $k=>$v) {
-                $i_value=$v->getProperty($idfield,NULL,'is_string');
+                $iValue=$v->getProperty($idField,NULL,'is_string');
                 if(strlen($relation)) {
                     $relObj=$v->getProperty($relation,'','is_string');
                     if(is_object($relObj)) {
-                        $i_label=$relObj->getProperty($labelfield,NULL,'is_object');
+                        /** @var VirtualEntity $relObj */
+                        $iLabel=$relObj->getProperty($labelField,NULL,'is_object');
                     } else {
-                        $i_label='';
+                        $iLabel='';
                     }//if(is_object($relObj))
                 } else {
-                    $i_label=$v->getProperty($labelfield,'','is_string');
+                    $iLabel=$v->getProperty($labelField,'','is_string');
                 }//if(strlen($relation))
                 if(isset($this->value)) {
-                    $i_val=$i_value==$this->value ? 1 : 0;
+                    if($this->multiple) {
+                        $iVal=in_array($iValue,$this->GetMultiSelectedValue()) ? 1 : 0;
+                    } else {
+                        $iVal=$iValue==$this->value ? 1 : 0;
+                    }//if($this->multiple)
                 } elseif(strlen($this->default_value_field)) {
-                    $i_val=$v->getProperty($this->default_value_field,0,'is_numeric')==1 ? 1 : 0;
+                    $iVal=$v->getProperty($this->default_value_field,0,'is_numeric')==1 ? 1 : 0;
                 } else {
-                    $i_val=0;
+                    $iVal=0;
                 }//if(isset($this->value))
                 if($this->disabled || $this->readonly) {
-                    $i_active=FALSE;
+                    $iActive=FALSE;
                 } else {
-                    $i_active=strlen($statefield) ? $v->getProperty($statefield,NULL,'is_string')==$activestate : TRUE;
+                    $iActive=strlen($stateField) ? $v->getProperty($stateField,NULL,'is_string')==$activeState : TRUE;
                 }//if($this->disabled || $this->readonly)
-                $result.="\t\t".'<li><input type="image" class="clsGCKBItem'.($i_active ? ' active' : ' disabled').'" data-id="'.$this->tag_id.'" data-val="'.$i_value.'" src="'.NApp::$appBaseUrl.AppConfig::GetValue('app_js_path').'/controls/images/transparent.gif" value="'.$i_val.'"><label class="clsGCKBLabel">'.$i_label.'</label></li>'."\n";
+                if($this->multiple && $this->multiResult) {
+                    $itemTagId=' id="'.$this->tag_id.'_'.$iValue.'"';
+                    $itemTagName=' name="'.(strlen($this->tag_name) ? $this->tag_name : $this->tag_id).'['.$iValue.']"';
+                } else {
+                    $itemTagId=$itemTagName='';
+                }
+                $result.="\t\t".'<li><input type="image"'.$itemTagId.$itemTagName.' class="clsGCKBItem'.($this->multiple && $this->multiResult ? ' postable' : '').($iActive ? ' active' : ' disabled').'" data-id="'.$this->tag_id.$hiddenTagSufix.'" data-val="'.$iValue.'" data-multiple="'.((int)$this->multiple).'" src="'.NApp::$appBaseUrl.AppConfig::GetValue('app_js_path').'/controls/images/transparent.gif" value="'.$iVal.'"><label class="clsGCKBLabel">'.$iLabel.'</label></li>'."\n";
             }//END foreach
         } else {
             $result.="\t\t<li><span class=\"clsGCKBBlank\">".Translate::GetLabel('no_elements')."</span></li>\n";
@@ -120,4 +149,35 @@ class GroupCheckBox extends Control {
         $result.=$this->GetActions();
         return $result;
     }//END protected function SetControl
+
+    /**
+     * Determines what kind of value to return.
+     *
+     * @return string|null
+     */
+    protected function GetValue(): ?string {
+        if($this->multiple) {
+            if(is_iterable($this->value)) {
+                $value=implode('|',$this->value);
+            } elseif(is_scalar($this->value)) {
+                $value=$this->value;
+            } else {
+                $value=NULL;
+            }
+        } else {
+            $value=$this->value;
+        }
+        return $value;
+    }//END protected function GetValue
+
+    protected function GetMultiSelectedValue() {
+        if(is_string($this->value)) {
+            $value=strlen($this->value) ? explode(self::MULTI_VALUE_SEPARATOR,$this->value) : [];
+        } elseif(is_array($this->value)) {
+            $value=$this->value;
+        } else {
+            $value=[];
+        }
+        return $value;
+    }//END protected function GetSelectedValue
 }//END class GroupCheckBox extends Control
