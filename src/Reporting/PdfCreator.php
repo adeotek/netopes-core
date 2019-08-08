07bd9ec475d7edd7cb97669b806910fe35cfb306
George Benjamin-Schonberger <george.benjamin@gmail.com>
2019-08-08 15:21:27 +0300

    FIX - PdfCreator fix

--- src/Reporting/PdfCreator.php
+++ src/Reporting/PdfCreator.php
@@ -416,7 +416,7 @@ class PdfCreator extends TCPDF {
      * @param array  $parameters
      * @return string
      */
-    protected function ReplacePlaceholders(string $content,array $parameters): string {
+    public function ReplacePlaceholders(string $content,array $parameters): string {
         $placeholders=[];
         if(preg_match_all($this->placeholdersRegExp,$content,$placeholders)) {
             foreach($placeholders[0] as $placeholder) {
@@ -424,14 +424,14 @@ class PdfCreator extends TCPDF {
             }//END foreach
         }//if(preg_match_all($this->placeholdersRegExp,$content,$placeholders))
         return $content;
-    }//END protected function ReplacePlaceholders
+    }//END public function ReplacePlaceholders
 
     /**
      * @param string $placeholder
      * @param array  $parameters
      * @return string
      */
-    protected function GetPlaceholderValue(string $placeholder,array $parameters): string {
+    public function GetPlaceholderValue(string $placeholder,array $parameters): string {
         $paramValue=get_array_value($parameters,$placeholder,NULL,'isset');
         if(is_array($paramValue)) {
             $value=get_array_param($paramValue,'value',NULL,'?is_string');
@@ -440,12 +440,15 @@ class PdfCreator extends TCPDF {
             }
             $tagType=strtolower(get_array_param($paramValue,'type','','is_notempty_string'));
             $label=get_array_param($paramValue,'label',NULL,'?is_string');
+            $style=get_array_param($paramValue,'style',NULL,'?is_string');
             switch($tagType) {
+                case 'table':
+                    return '<table'.(strlen($style) ? ' style="'.$style.'"' : '').'><tr><td>'.(strlen($label) ? $label.':&nbsp;' : '').'</td><td>'.$value.'</td></tr></table>';
                 case 'tr':
                     return '<tr><td>'.(strlen($label) ? $label.':&nbsp;' : '').'</td><td>'.$value.'</td></tr>';
                 case 'div':
                 case 'span':
-                    return '<'.$tagType.'>'.(strlen($label) ? $label.':&nbsp;' : '').$value.'</'.$tagType.'>';
+                    return '<'.$tagType.(strlen($style) ? ' style="'.$style.'"' : '').'>'.(strlen($label) ? $label.':&nbsp;' : '').$value.'</'.$tagType.'>';
                 case 'no_tag':
                     return (strlen($label) ? $label.':&nbsp;' : '').$value;
                 default:
@@ -453,5 +456,5 @@ class PdfCreator extends TCPDF {
             }//END switch
         }//if(is_array($paramValue))
         return (is_scalar($paramValue) ? $paramValue : NULL) ?? '';
-    }//END protected function GetPlaceholderValue
+    }//END public function GetPlaceholderValue
 }//END class PdfCreator extends TCPDF
